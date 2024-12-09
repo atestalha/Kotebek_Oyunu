54c03fb566b4da31509395ce904210130bf93c6a
talha <atst>
2024-12-09 10:18:26 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace KÖSTEBEK_OYUNU
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(8, 9);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(935, 528);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 548);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KÖSTEBEK_OYUNU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int puan = 0;
+        Random rdm= new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            for(int i = 1; i < 85; i++)
+            {
+                Button btn= new Button();
+                btn.BackColor = Color.Red;
+                btn.ForeColor=Color.Black;
+                btn.Height = 50;
+                btn.Width = 50;
+                btn.Text = i.ToString();
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int rastgele = rdm.Next(1, 85);
+            foreach(var a in flowLayoutPanel1.Controls)
+            {
+                Button btn = a as Button;
+                if (btn.Text == rastgele.ToString())
+                {
+                    btn.BackColor = Color.Red;
+                    btn.Click += new EventHandler(btn_click);
+                }
+                else
+                {
+                    btn.BackColor = Color.Black;
+                }
+            }
+        }
+        void btn_click(object sender, EventArgs e)
+        {
+            Button btn=sender as Button;
+            if(btn.BackColor == Color.Red)
+            {
+                puan++;
+            }
+            else
+            {
+                puan--;
+            }
+            this.Text=puan.ToString();
+        }
+    }
+}
